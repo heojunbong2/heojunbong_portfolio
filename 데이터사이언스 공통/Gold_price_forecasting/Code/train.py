2ee0fbfa518317a2d570a6441c837fb9b37e55e1
heojunbong2 <168062535+heojunbong2@users.noreply.github.com>
2024-08-25 04:37:24 +0900

    Add files via upload

---
+++ 데이터사이언스 공통/Gold_price_forecasting/Code/train.py
@@ -0,0 +1,63 @@
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+import pickle
+import pandas as pd
+import FinanceDataReader as fdr
+
+import hmm
+# you can create your own function or other .py file to be used here.
+
+
+
+def main():
+
+    # ----------------Do not change this part ---------------------------------------
+    train_start = '2010-01-01'
+    train_end = '2024-05-16'
+
+    # 원자재 가격
+    GC = fdr.DataReader('GC=F', train_start, train_end) # 금 선물 (COMEX)
+    CL = fdr.DataReader('CL=F', train_start, train_end) # WTI유 선물 Crude Oil (NYMEX)
+    BZ = fdr.DataReader('BZ=F', train_start, train_end) # 브렌트유 선물 Brent Oil (NYMEX)
+    NG = fdr.DataReader('NG=F', train_start, train_end) # 천연가스 선물 (NYMEX)
+    SI = fdr.DataReader('SI=F', train_start, train_end) # 은 선물 (COMEX)
+    HG = fdr.DataReader('HG=F', train_start, train_end) # 구리 선물 (COMEX)
+
+    # 환율
+    USDKRW = fdr.DataReader('USD/KRW', train_start, train_end) # 달러 원화
+    USDEUR = fdr.DataReader('USD/EUR', train_start, train_end) # 달러 유로화
+    USDCNY = fdr.DataReader('USD/CNY', train_start, train_end) # 달러 위엔화
+
+    # 암호화폐 데이터
+    BTCUSD= fdr.DataReader('BTC/USD', train_start, train_end) # 비트코인/달러
+    ETHUSD = fdr.DataReader('ETH/USD', train_start, train_end) # 이더리움/달러
+    #-----------------------------------------------------------------------------
+    
+    # TODO: preprocessing / model training
+    #ㅗhmm에서 사용할 데이터 
+    data_hmm = pd.DataFrame({
+        'GC': GC["Close"],
+        'CL': CL['Close'],
+        'BZ': BZ['Close'],
+        'NG': NG['Close'],
+        'SI': SI['Close'],
+        'HG': HG['Close'],
+        'USDKRW': USDKRW['Close'],
+        'USDEUR': USDEUR['Close'],
+        'USDCNY': USDCNY['Close'],
+        'BTCUSD': BTCUSD['Close'],
+        'ETHUSD': ETHUSD['Close']
+    }).dropna()
+    
+    hmm.hmm_train(data_hmm)
+
+    
+    
+    # TODO: save a file of your model
+    
+       
+    
+
+
+if __name__ == "__main__":
+    main()
